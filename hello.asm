d6e5ac75ebd0473aad000c857bea70b0b789f337
frankeggink <freggink@gmail.com>
2019-10-30 12:10:34 +0100

    Add files via upload

---
+++ hello.asm
@@ -0,0 +1,18 @@
+
+EOS:         .EQU    $0000            	;End of string
+
+
+.org $0000
+start:	
+	SET R1, HELP_MSG_1
+next:	
+	LD AC, @R1
+	OUT
+	INC R1
+	BGT next
+	BRA start
+	
+HELP_MSG_1: .BYTE "Hello World!\n",EOS
+	
+	
+.end	
